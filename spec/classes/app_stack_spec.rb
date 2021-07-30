267d5860ba827e69bf9a21d2c7f062093f512043
Andrew Wrenn <awrenn53@gmail.com>
2021-07-30 07:16:52 -0700

    Update tests for HDP

--- spec/classes/app_stack_spec.rb
+++ spec/classes/app_stack_spec.rb
@@ -4,6 +4,7 @@ describe 'hdp::app_stack' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:params) { {'dns_name' => 'hdp.test.com'} }
 
       context 'with defaults' do
         it { is_expected.to compile.with_all_deps }
@@ -11,7 +12,7 @@ describe 'hdp::app_stack' do
         it { is_expected.to  contain_class('docker').with_log_driver('journald') }
         it { is_expected.to  contain_class('docker::compose').with_ensure('present') }
         it { is_expected.to  contain_file('/opt/puppetlabs/hdp').with_ensure('directory') }
-        it { is_expected.to  contain_file('/opt/puppetlabs/hdp/docker-compose.yaml').with_content(%r{puppet\/hdp-ingest-queue:\d+\.\d+\.\d+(-[a-z]+(\.[0-9]+)?)?"$}) }
+        it { is_expected.to  contain_file('/opt/puppetlabs/hdp/docker-compose.yaml').with_content(%r{NAME=hdp\.test\.com}) }
         it { is_expected.to  contain_docker_compose('hdp').with_compose_files(['/opt/puppetlabs/hdp/docker-compose.yaml']) }
       end
     end
