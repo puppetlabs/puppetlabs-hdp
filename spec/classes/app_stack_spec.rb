aaacfddd69e679077d2eb27bf8b18e08e58cff4b
Gene Liverman <gene.liverman+14713432@puppet.com>
2021-08-04 12:03:06 -0400

    Testing and formatting updates

--- spec/classes/app_stack_spec.rb
+++ spec/classes/app_stack_spec.rb
@@ -12,14 +12,45 @@ describe 'hdp::app_stack' do
         it { is_expected.to  contain_class('docker').with_log_driver('journald') }
         it { is_expected.to  contain_class('docker::compose').with_ensure('present') }
         it { is_expected.to  contain_file('/opt/puppetlabs/hdp').with_ensure('directory') }
-        it { is_expected.to  contain_file('/opt/puppetlabs/hdp/docker-compose.yaml').with_content(%r{NAME=hdp\.test\.com}) }
         it {
           is_expected.to contain_docker_compose('hdp')
             .with_compose_files(['/opt/puppetlabs/hdp/docker-compose.yaml'])
+            .that_requires('File[/opt/puppetlabs/hdp/docker-compose.yaml]')
             .that_subscribes_to('File[/opt/puppetlabs/hdp/docker-compose.yaml]')
         }
-        it { is_expected.to contain_file('/opt/puppetlabs/hdp/docker-compose.yaml').with_content(%r{- "80:80"}) }
-        it { is_expected.to contain_file('/opt/puppetlabs/hdp/docker-compose.yaml').without_content(%r{- "443:443"}) }
+        it {
+          is_expected.to contain_file('/opt/puppetlabs/hdp/docker-compose.yaml')
+            .with_owner('root')
+            .with_group('docker')
+            .with_content(%r{NAME=hdp\.test\.com})
+            .with_content(%r{- "80:80"})
+            .without_content(%r{- "443:443"})
+        }
+        dir_list = [
+          '/opt/puppetlabs/hdp',
+          '/opt/puppetlabs/hdp/minio',
+          '/opt/puppetlabs/hdp/minio/config',
+          '/opt/puppetlabs/hdp/minio/data',
+          '/opt/puppetlabs/hdp/minio/data/facts',
+          '/opt/puppetlabs/hdp/redis',
+          '/opt/puppetlabs/hdp/ssl',
+        ]
+
+        dir_list.each do |d|
+          it {
+            is_expected.to contain_file(d)
+              .with_ensure('directory')
+              .with_owner('11223')
+              .with_group('11223')
+          }
+        end
+
+        it {
+          is_expected.to contain_file('/opt/puppetlabs/hdp/elastic')
+            .with_ensure('directory')
+            .with_owner('1000')
+            .with_group('1000')
+        }
       end
 
       context 'with ui tls enabled' do
@@ -40,16 +71,28 @@ describe 'hdp::app_stack' do
         end
 
         it { is_expected.to compile.with_all_deps }
-        it { is_expected.to  contain_file('/tmp/ui-cert.key').that_comes_before('Docker_compose[hdp]') }
-        it { is_expected.to  contain_file('/tmp/ui-cert.pem').that_comes_before('Docker_compose[hdp]') }
         it {
-          is_expected.to contain_docker_compose('hdp').that_subscribes_to(
-            [
-              'File[/tmp/ui-cert.key]',
-              'File[/tmp/ui-cert.pem]',
-              'File[/opt/puppetlabs/hdp/docker-compose.yaml]',
-            ],
-          )
+          is_expected.to contain_docker_compose('hdp')
+            .with_compose_files(['/opt/puppetlabs/hdp/docker-compose.yaml'])
+            .that_requires(
+              [
+                'File[/tmp/ui-cert.key]',
+                'File[/tmp/ui-cert.pem]',
+                'File[/opt/puppetlabs/hdp/docker-compose.yaml]',
+              ],
+            )
+            .that_subscribes_to(
+              [
+                'File[/tmp/ui-cert.key]',
+                'File[/tmp/ui-cert.pem]',
+                'File[/opt/puppetlabs/hdp/docker-compose.yaml]',
+              ],
+            )
+        }
+        it {
+          is_expected.to contain_file('/opt/puppetlabs/hdp/docker-compose.yaml')
+            .with_content(%r{- "80:80"})
+            .with_content(%r{- "443:443"})
         }
         it { is_expected.to contain_file('/opt/puppetlabs/hdp/docker-compose.yaml').with_content(%r{- "80:80"}) }
         it { is_expected.to contain_file('/opt/puppetlabs/hdp/docker-compose.yaml').with_content(%r{- "443:443"}) }
